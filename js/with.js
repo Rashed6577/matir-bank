76e84ce182d335a34a72c090d4ba08a9a90f6c3e
Rashed <rashedulhoqueceo@gmail.com>
2023-08-15 23:34:36 +0600

    complated

--- js/with.js
+++ js/with.js
@@ -2,21 +2,30 @@ document.getElementById('with-btn').addEventListener('click', function(){
     const withInput = document.getElementById('with-input');
     const withValueString = withInput.value;
     const withValueNum = parseFloat(withValueString);
+    if(isNaN(withValueNum)){
+        alert('Please provide a valid amount');
+        return;
+    }
 
     const withAmount = document.getElementById('with-amount');
     const withAmountString = withAmount.innerText;
     const withAmountNum = parseFloat(withAmountString);
+    withInput.value = '';
 
-    withAmount.innerText = withAmountNum + withValueNum;
 
     const balance = document.getElementById('balance-amount');
     const balanceAmountString = balance.innerText;
     const balanceAmountNum = parseFloat(balanceAmountString);
 
+    if(withValueNum > balanceAmountNum){
+        alert('balance not found');
+        return;
+    }
+
     balance.innerText = balanceAmountNum - withValueNum;
 
+    withAmount.innerText = withAmountNum + withValueNum;
 
-
-    withInput.value = '';
+    
 
 })
